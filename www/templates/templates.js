ce924faaa0810f9f8e7388af926ac3b3f5ba3b78
Daniel Sellers <daniel@designfrontier.net>
2014-01-10 14:31:26 -0700

    updated the templates around a nav system

--- www/templates/templates.js
+++ www/templates/templates.js
@@ -6,7 +6,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
   
 
 
-  return "<div id=\"quote\" class=\"quote-color serif large quote\">\r\n    <textarea class=\"new-quote quote-input serif\" placeholder=\"Enter quote here&hellip;\" name=\"quote-text\"></textarea>\r\n    <input type=\"text\" class=\"quote-input serif small-pad-left\" name=\"quote-source\" placeholder=\"Source&hellip;\">\r\n</div>\r\n\r\n\r\n<div class=\"footer nav\">\r\n    <div class=\"button active left\"><</div>\r\n    <div class=\"button main active center js-save\">save</div>\r\n    <div class=\"button active right-1\" id=\"camera\">photo</div>\r\n</div>";
+  return "<div id=\"quote\" class=\"quote-color serif large quote\">\r\n    <textarea class=\"new-quote quote-input serif\" placeholder=\"Enter quote here&hellip;\" name=\"quote-text\"></textarea>\r\n    <input type=\"text\" class=\"quote-input serif small-pad-left\" name=\"quote-source\" placeholder=\"Source&hellip;\">\r\n</div>\r\n\r\n\r\n<div class=\"footer nav\">\r\n    <div class=\"button active left nav-main\"><</div>\r\n    <div class=\"button main active center js-save\">save</div>\r\n    <div class=\"button active right-1\" id=\"camera\">photo</div>\r\n</div>";
   });
 templates['main'] = template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
